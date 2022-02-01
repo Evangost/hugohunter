84a2f316b23f81e69355900c4ef7c21d58d761da
Evangost <rekato1990@gmail.com>
2022-02-01 18:14:27 +0200

    animation in process

--- src/js/index.js
+++ src/js/index.js
@@ -163,3 +163,20 @@ $(function () {
         });
     }
 });
+
+/*Counter*/
+$(document).ready(function() {
+    $('.numbers span').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
+
